a60287814551210283fa570070b5053c98d32a9c
Daniel Torres <danstorre@gmail.com>
2022-11-07 21:12:33 -0400

    StarTimer init does not send any message to infra.

--- LifeCoachTests/StartTimerUseCase.swift
+++ LifeCoachTests/StartTimerUseCase.swift
@@ -1,39 +1,18 @@
 import XCTest
 
-class TimerStarter {
-    private let timer: TimerSpy
-    init(timer: TimerSpy) {
-        self.timer = timer
-    }
-    
-    func startTimer() {
-        timer.start()
-    }
+class StarTimer {
+    init(infrastructure: InfraSpy) {}
 }
 
-class TimerSpy {
-    var startMessageCount = 0
-    
-    func start() {
-        startMessageCount += 1
-    }
+class InfraSpy {
+    var countMessages = 0
 }
 
 final class StartTimerUseCase: XCTestCase {
-
-    func test_init_doesNotSendsMessageToTimer() {
-        let timer = TimerSpy()
-        let _ = TimerStarter(timer: timer)
+    func test_init_doesNotSendAnyMessageToInfra() {
+        let infrastructure = InfraSpy()
+        let _ = StarTimer(infrastructure: infrastructure)
         
-        XCTAssertEqual(timer.startMessageCount, 0)
-    }
-    
-    func test_start_sendsStartMessageToTimer() {
-        let timer = TimerSpy()
-        let sut = TimerStarter(timer: timer)
-        
-        sut.startTimer()
-        
-        XCTAssertEqual(timer.startMessageCount, 1)
+        XCTAssertEqual(infrastructure.countMessages, 0)
     }
 }
