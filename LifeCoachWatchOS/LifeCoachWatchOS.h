4e29987d8e0874570555646129582dc6eb982ea7
Daniel Torres <danstorre@gmail.com>
2022-11-21 14:13:18 -0400

    Creates LifeCoachWatchOS framework.

---
+++ LifeCoachWatchOS/LifeCoachWatchOS.h
@@ -0,0 +1,18 @@
+//
+//  LifeCoachWatchOS.h
+//  LifeCoachWatchOS
+//
+//  Created by Daniel Torres on 11/21/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LifeCoachWatchOS.
+FOUNDATION_EXPORT double LifeCoachWatchOSVersionNumber;
+
+//! Project version string for LifeCoachWatchOS.
+FOUNDATION_EXPORT const unsigned char LifeCoachWatchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LifeCoachWatchOS/PublicHeader.h>
+
+
