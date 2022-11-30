558f362572214843bf43c8071abaacd5ae2d60c8
Daniel Torres <danstorre@gmail.com>
2022-11-30 11:49:19 -0400

    move helpers into an extension.

--- LifeCoachWatchOS/Timer Feature/Timer UI/TimerViewModel.swift
+++ LifeCoachWatchOS/Timer Feature/Timer UI/TimerViewModel.swift
@@ -15,32 +15,3 @@ class TimerViewModel: ObservableObject {
         timerString = formatter.string(from: startDate, to: endDate)!
     }
 }
-
-extension String {
-    public static let defaultPomodoroTimerString = "25:00"
-}
-
-extension TimeInterval {
-    public static var pomodoroInSeconds: TimeInterval {
-        1500
-    }
-    
-    public static var breakInSeconds: TimeInterval {
-        300
-    }
-}
-
-extension Date {
-    public func adding(seconds: TimeInterval) -> Date {
-        return self + seconds
-    }
-}
-
-
-func makeTimerFormatter() -> DateComponentsFormatter {
-    let dateFormatter = DateComponentsFormatter()
-    dateFormatter.zeroFormattingBehavior = .pad
-    dateFormatter.allowedUnits = [.minute, .second]
-    
-    return dateFormatter
-}
