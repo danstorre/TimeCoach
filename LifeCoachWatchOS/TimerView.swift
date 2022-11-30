a54a4fc189588ed6f7579407d0898c4c2c148e8e
Daniel Torres <danstorre@gmail.com>
2022-11-30 10:19:35 -0400

    move `TimerViewComposer` to its own file.

--- LifeCoachWatchOS/TimerView.swift
+++ LifeCoachWatchOS/TimerView.swift
@@ -1,35 +1,6 @@
 import SwiftUI
 import Combine
 
-public final class TimerViewComposer {
-    public static func createTimer(
-        timerLoader: @escaping () -> AnyPublisher<ElapsedSeconds, Error>,
-        playHandler: (() -> Void)? = nil,
-        skipHandler: (() -> Void)? = nil,
-        stopHandler: (() -> Void)? = nil
-    ) -> TimerView {
-        let presentationAdapter = TimerLoaderPresentationAdapter(loader: timerLoader)
-     
-        let didPlay = {
-            playHandler?()
-            presentationAdapter.startTimer()
-        }
-        
-        let viewModel = TimerViewModel()
-        
-        presentationAdapter.presenter = viewModel
-        
-        let timer = TimerView(
-            timerViewModel: viewModel,
-            playHandler: didPlay,
-            skipHandler: skipHandler,
-            stopHandler: stopHandler
-        )
-        
-        return timer
-    }
-}
-
 public struct TimerView: View {
     @ObservedObject var timerViewModel: TimerViewModel
     
