97be869c32ede930e1abedee477ac7eada73b48d
Daniel Torres <danstorre@gmail.com>
2022-11-29 21:40:26 -0400

    adds timer ui integration tests.

---
+++ LifeCoachWatchOSTests/TimerUIIntegrationTests.swift
@@ -0,0 +1,12 @@
+@testable import LifeCoachWatchOS
+import XCTest
+
+final class TimerUIIntegrationTests: XCTestCase {
+    func test_zero() throws {
+        XCTFail("Tests not yet implemented in TimerUIIntegrationTests")
+    }
+}
+
+
+
+
