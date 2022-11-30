40ab0fe05ee1b61b52edb7d80c8d7952bd6ad33f
Daniel Torres <danstorre@gmail.com>
2022-11-30 13:11:11 -0400

    sends correct break times when start timer is called twice.

--- LifeCoachTests/StartTimerUseCasesTests.swift
+++ LifeCoachTests/StartTimerUseCasesTests.swift
@@ -3,15 +3,30 @@ import LifeCoach
 
 class StarTimer {
     private let timer: TimerSpy
+    private var mode: TimerMode = .pomodoroTime
+    private var isPomodoro = true
+    
+    enum TimerMode {
+        case pomodoroTime
+        case breakTime
+    }
     
     init(timer: TimerSpy) {
         self.timer = timer
     }
     
     func startTimer(from startDate: Date = .now) {
-        let pomodoroTime = startDate.adding(seconds: .pomodoroInSeconds)
+        let endTime: Date
+        if isPomodoro {
+            endTime = startDate.adding(seconds: .pomodoroInSeconds)
+        } else {
+            endTime = startDate.adding(seconds: .breakInSeconds)
+        }
+        
+        isPomodoro = !isPomodoro
+        
         timer.startCountdown(from: startDate,
-                             endDate: pomodoroTime)
+                             endDate: endTime)
     }
 }
 
@@ -50,6 +65,18 @@ final class StartTimerUseCase: XCTestCase {
         
         XCTAssertEqual(spy.startDatesReceived, [now])
         XCTAssertEqual(spy.endDatesReceived, [pomodoroTime])
+    }
+    
+    func test_startTimer_twice_sendsCorrectBreakTimes() {
+        let now = Date.now
+        let pomodoroTime = Date.now.adding(seconds: .pomodoroInSeconds)
+        let breakTime = Date.now.adding(seconds: .breakInSeconds)
+        let (sut, spy) = makeSUT()
+        sut.startTimer(from: now)
+        sut.startTimer(from: now)
+        
+        XCTAssertEqual(spy.startDatesReceived, [now, now])
+        XCTAssertEqual(spy.endDatesReceived, [pomodoroTime, breakTime])
     }
     
     // MARK: - helpers
