ca052acdc59afe4df47d79ef17f485cc8295c576
Daniel Torres <danstorre@gmail.com>
2022-11-30 13:02:16 -0400

    moves extension helpers to shared extensions on mac target.

--- LifeCoach/ExtensionHelpers.swift
+++ LifeCoach/ExtensionHelpers.swift
@@ -1,9 +1,5 @@
 import Foundation
 
-extension String {
-    public static let defaultPomodoroTimerString = "25:00"
-}
-
 extension TimeInterval {
     public static var pomodoroInSeconds: TimeInterval {
         1500
---
+++ LifeCoachWatchOS/Timer Feature/Timer UI/Helpers/Timer+DefaultStrings.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+extension String {
+    public static let defaultPomodoroTimerString = "25:00"
+}
