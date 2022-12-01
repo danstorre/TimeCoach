57172889a9ebbb2dc1c049bc6f7da663c548d551
Daniel Torres <danstorre@gmail.com>
2022-12-01 18:19:10 -0400

    configures Timer Label with custom font and color.

--- LifeCoachWatchOS/Timer Feature/UI/TimerView.swift
+++ LifeCoachWatchOS/Timer Feature/UI/TimerView.swift
@@ -1,8 +1,25 @@
 import SwiftUI
 import Combine
 
+struct TimerLabelStyle: LabelStyle {
+    let isLuminanceReduced: Bool
+    let customFont: String?
+    
+  func makeBody(configuration: Configuration) -> some View {
+    configuration
+          .title
+          .foregroundColor(.red)
+          .font(Font.custom(customFont ?? "", size: 40))
+          .opacity(isLuminanceReduced ? 0.75 : 1.0)
+  }
+}
+
+
 public struct TimerView: View {
+    @Environment(\.isLuminanceReduced) var isLuminanceReduced
+    
     @ObservedObject var timerViewModel: TimerViewModel
+    private let customFont: String?
     
     private var togglePlayback: (() -> Void)?
     private var skipHandler: (() -> Void)?
@@ -11,17 +28,21 @@ public struct TimerView: View {
     public init(timerViewModel: TimerViewModel,
          togglePlayback: (() -> Void)? = nil,
          skipHandler: (() -> Void)? = nil,
-         stopHandler: (() -> Void)? = nil
+         stopHandler: (() -> Void)? = nil,
+         customFont: String? = nil
     ) {
         self.timerViewModel = timerViewModel
         self.togglePlayback = togglePlayback
         self.skipHandler = skipHandler
         self.stopHandler = stopHandler
+        self.customFont = customFont
     }
     
     public var body: some View {
         VStack {
-            Text(timerViewModel.timerString)
+            Label(timerViewModel.timerString, systemImage: "")
+                .labelStyle(TimerLabelStyle(isLuminanceReduced: isLuminanceReduced,
+                                            customFont: customFont))
                 .accessibilityIdentifier(Self.timerLabelIdentifier)
             
             Button.init(action: togglePlayback ?? {}) {
