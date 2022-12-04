fd9a441b66efc512ce8bce4ed05241b0f52f0f4c
Daniel Torres <danstorre@gmail.com>
2022-12-04 17:19:16 -0400

    move `ToggleStrategy` to its own file.

--- TimeCoach Watch App/Timer Composition/TimerViewComposer.swift
+++ TimeCoach Watch App/Timer Composition/TimerViewComposer.swift
@@ -2,48 +2,6 @@ import Combine
 import LifeCoachWatchOS
 import LifeCoach
 
-class ToggleStrategy {
-    private var play: Bool = false
-    private let start: (() -> Void)?
-    private let pause: (() -> Void)?
-    private let skip: (() -> Void)?
-    private let stop: (() -> Void)?
-    
-    init(start: (() -> Void)?, pause: (() -> Void)?, skip: (() -> Void)?, stop: (() -> Void)?) {
-        self.start = start
-        self.pause = pause
-        self.skip = skip
-        self.stop = stop
-    }
-    
-    func toggle() {
-        if play {
-            pause?()
-        } else {
-            start?()
-        }
-        play = !play
-    }
-    
-    func skipHandler() {
-        if play {
-            pause?()
-            play = false
-        }
-        
-        skip?()
-    }
-    
-    func stopHandler() {
-        if play {
-            pause?()
-            play = false
-        }
-        
-        stop?()
-    }
-}
-
 public final class TimerViewComposer {
     public static func createTimer(
         customFont: String,
