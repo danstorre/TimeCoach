e69de36bf47bb87b5f1195c841e7425220747c47
Daniel Torres <danstorre@gmail.com>
2022-12-06 10:38:44 -0400

    try to make `TimelineView` Inspectable

--- TimeCoach Watch AppTests/TimerUI/TimerUIIntegrationTests.swift
+++ TimeCoach Watch AppTests/TimerUI/TimerUIIntegrationTests.swift
@@ -7,6 +7,15 @@ import LifeCoachWatchOS
 import TimeCoach_Watch_App
 
 extension TimerView: Inspectable { }
+extension TimelineView: Inspectable {
+    public var entity: ViewInspector.Content.InspectableEntity {
+        .view
+    }
+    
+    public func extractContent(environmentObjects: [AnyObject]) throws -> Any {
+        0
+    }
+}
 
 final class TimerUIIntegrationTests: XCTestCase {
     func test_onInitialLoad_shouldDisplayCorrectCustomFont() {
