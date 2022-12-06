b4d87ef93ba10c65dd870e53cdece257169196d8
Daniel Torres <danstorre@gmail.com>
2022-12-05 22:06:05 -0400

    move components to their own files.

--- LifeCoachWatchOS/Timer Feature/UI/TimerControls.swift
+++ LifeCoachWatchOS/Timer Feature/UI/TimerControls.swift
@@ -1,28 +1,4 @@
 import SwiftUI
-import Combine
-import LifeCoach
-
-public struct TimerText: View {
-    @Environment(\.isLuminanceReduced) var isLuminanceReduced
-    @ObservedObject var timerViewModel: TimerViewModel
-    public var customFont: String?
-    
-    public init(timerViewModel: TimerViewModel, customFont: String? = nil) {
-        self.timerViewModel = timerViewModel
-        self.customFont = customFont
-    }
-    
-    public var body: some View {
-        Label(title: { Text(timerViewModel.timerString) }, icon: {})
-            .labelStyle(TimerLabelStyle(isLuminanceReduced: isLuminanceReduced,
-                                        customFont: customFont))
-            .accessibilityIdentifier(Self.timerLabelIdentifier)
-    }
-}
-
-extension TimerText {
-    public static let timerLabelIdentifier = "timerLabelIdentifier"
-}
 
 public struct TimerControls: View {
     @Environment(\.isLuminanceReduced) var isLuminanceReduced
---
+++ LifeCoachWatchOS/Timer Feature/UI/TimerText.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+import Combine
+import LifeCoach
+
+public struct TimerText: View {
+    @Environment(\.isLuminanceReduced) var isLuminanceReduced
+    @ObservedObject var timerViewModel: TimerViewModel
+    public var customFont: String?
+    
+    public init(timerViewModel: TimerViewModel, customFont: String? = nil) {
+        self.timerViewModel = timerViewModel
+        self.customFont = customFont
+    }
+    
+    public var body: some View {
+        Label(title: { Text(timerViewModel.timerString) }, icon: {})
+            .labelStyle(TimerLabelStyle(isLuminanceReduced: isLuminanceReduced,
+                                        customFont: customFont))
+            .accessibilityIdentifier(Self.timerLabelIdentifier)
+    }
+}
+
+extension TimerText {
+    public static let timerLabelIdentifier = "timerLabelIdentifier"
+}
