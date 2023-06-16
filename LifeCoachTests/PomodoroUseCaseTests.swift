15449b318f963526fc5fb83736358b45f92770db
Dan Torres <danstorre@gmail.com>
2023-06-16 15:50:59 -0400

    sends message to timer countdown when pomodor timer starts.

--- LifeCoachTests/PomodoroUseCaseTests.swift
+++ LifeCoachTests/PomodoroUseCaseTests.swift
@@ -1,14 +1,23 @@
 import XCTest
 
 class PomodoroTimer {
+    private let timer: TimerSpy
     
     init(timer: TimerSpy) {
-        
+        self.timer = timer
+    }
+    
+    func start() {
+        timer.startCountdown()
     }
 }
 
 class TimerSpy {
     private(set) var getTimeCallCount = 0
+    
+    func startCountdown() {
+        getTimeCallCount += 1
+    }
 }
 
 final class PomodoroUseCaseTests: XCTestCase {
@@ -17,5 +26,14 @@ final class PomodoroUseCaseTests: XCTestCase {
         let timerHelper = TimerSpy()
         let _ = PomodoroTimer(timer: timerHelper)
         XCTAssertEqual(timerHelper.getTimeCallCount, 0)
+    }
+    
+    func test_start_sendsStartMessageToTimerCountdown() {
+        let timerHelper = TimerSpy()
+        let pomodoroTimer = PomodoroTimer(timer: timerHelper)
+        
+        pomodoroTimer.start()
+        
+        XCTAssertEqual(timerHelper.getTimeCallCount, 1)
     }
 }
