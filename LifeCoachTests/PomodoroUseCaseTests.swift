7e70cc3312d6402f83d6c2aed50eed8cd20fc7e1
Dan Torres <danstorre@gmail.com>
2023-06-16 16:05:16 -0400

    delivers timer error on timer countdown error.

--- LifeCoachTests/PomodoroUseCaseTests.swift
+++ LifeCoachTests/PomodoroUseCaseTests.swift
@@ -2,21 +2,36 @@ import XCTest
 
 class PomodoroTimer {
     private let timer: TimerSpy
+    private let timeReceiver: (Error) -> Void
     
-    init(timer: TimerSpy) {
+    enum Error: Swift.Error {
+        case timerError
+    }
+    
+    init(timer: TimerSpy, timeReceiver: @escaping (Error) -> Void) {
         self.timer = timer
+        self.timeReceiver = timeReceiver
     }
     
     func start() {
-        timer.startCountdown()
+        timer.startCountdown() { [unowned self] error in
+            self.timeReceiver(.timerError)
+        }
     }
 }
 
 class TimerSpy {
     private(set) var startCountdownCallCount = 0
     
-    func startCountdown() {
+    private var startCountdownCompletions = [(Error) -> Void]()
+    
+    func startCountdown(completion: @escaping (Error) -> Void) {
         startCountdownCallCount += 1
+        startCountdownCompletions.append(completion)
+    }
+    
+    func failsTimerWith(error: NSError, at index: Int = 0) {
+        startCountdownCompletions[index](error)
     }
 }
 
@@ -35,14 +50,33 @@ final class PomodoroUseCaseTests: XCTestCase {
         XCTAssertEqual(spy.startCountdownCallCount, 1)
     }
     
+    func test_start_deliversTimerErrorOnTimerCountdownError() {
+        var receivedError: PomodoroTimer.Error?
+        let (sut, spy) = makeSUT(timeReceiver: { error in
+            receivedError = error
+        })
+        
+        sut.start()
+        
+        spy.failsTimerWith(error: anyNSError())
+        
+        XCTAssertEqual(receivedError, .timerError)
+    }
+    
     // MARK: - Helpers
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (PomodoroTimer, TimerSpy) {
+    private func makeSUT(timeReceiver: ((PomodoroTimer.Error) -> Void)? = nil,
+                         file: StaticString = #filePath,
+                         line: UInt = #line) -> (PomodoroTimer, TimerSpy) {
         let spy = TimerSpy()
-        let sut = PomodoroTimer(timer: spy)
+        let sut = PomodoroTimer(timer: spy, timeReceiver: timeReceiver ?? { _ in })
         
         trackForMemoryLeak(instance: spy, file: file, line: line)
         trackForMemoryLeak(instance: sut, file: file, line: line)
         
         return (sut, spy)
+    }
+    
+    private func anyNSError() -> NSError {
+        NSError(domain: "any", code: 1)
     }
 }
