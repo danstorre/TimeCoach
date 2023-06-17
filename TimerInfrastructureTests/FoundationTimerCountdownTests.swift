ada7cdc4940382c12eb14a4947a3d080513df57a
Dan Torres <danstorre@gmail.com>
2023-06-16 21:20:26 -0400

    inserts two sets on countdown timer to move to the next one when reaching threshold, fixes `test_start_deliversTwoSecondsElapsedFromTheSetOfStartingSecondsAndChangesStateToRunning`

--- TimerInfrastructureTests/FoundationTimerCountdownTests.swift
+++ TimerInfrastructureTests/FoundationTimerCountdownTests.swift
@@ -10,14 +10,18 @@ class FoundationTimerCountdown {
     }
     
     private(set) var state: TimerState = .pause
-    private let startingSet: LocalElapsedSeconds
+    private var setA: LocalElapsedSeconds
+    private var setB: LocalElapsedSeconds
+    private var currentSet: LocalElapsedSeconds
     private var elapsedTimeInterval: TimeInterval = 0
     private var timerDelivery: StartCoundownCompletion?
     
     private var currentTimer: Timer?
     
-    init(startingSet: LocalElapsedSeconds) {
-        self.startingSet = startingSet
+    init(startingSet: LocalElapsedSeconds, nextSet: LocalElapsedSeconds) {
+        self.setA = startingSet
+        self.setB = nextSet
+        self.currentSet = startingSet
     }
     
     func startCountdown(completion: @escaping StartCoundownCompletion) {
@@ -34,8 +38,18 @@ class FoundationTimerCountdown {
     
     @objc
     private func elapsedCompletion() {
+        let endDate = currentSet.endDate.adding(seconds: -elapsedTimeInterval)
+        
+        guard endDate.timeIntervalSince(currentSet.startDate) > 0 else {
+            setA = currentSet
+            currentSet = setB
+            setB = setA
+            return
+        }
+        
         elapsedTimeInterval += 1
-        let elapsed = startingSet.addingElapsedSeconds(elapsedTimeInterval)
+        
+        let elapsed = currentSet.addingElapsedSeconds(elapsedTimeInterval)
         timerDelivery?(.success(elapsed))
     }
     
@@ -53,21 +67,22 @@ private extension LocalElapsedSeconds {
 final class FoundationTimerCountdownTests: XCTestCase {
     
     func test_init_stateIsPaused() {
-        let sut = makeSUT(startingSet: createAnyTimerSet())
+        let sut = makeSUT(startingSet: createAnyTimerSet(), nextSet: createAnyTimerSet())
         XCTAssertEqual(sut.state, .pause)
     }
     
     func test_start_deliversOneSecondElapsedFromTheSetOfStartingSecondsAndChangesStateToRunning() {
         let startSet = createAnyTimerSet()
-        let sut = makeSUT(startingSet: startSet)
+        let sut = makeSUT(startingSet: startSet, nextSet: createAnyTimerSet())
         
         expect(sut: sut, toDeliver: [startSet.addingElapsedSeconds(1)],
                andChangesStateTo: .running)
     }
     
     func test_start_deliversTwoSecondsElapsedFromTheSetOfStartingSecondsAndChangesStateToRunning() {
-        let startSet = createAnyTimerSet()
-        let sut = makeSUT(startingSet: startSet)
+        let fixedDate = Date()
+        let startSet = createTimerSet(0, startDate: fixedDate, endDate: fixedDate.adding(seconds: 2))
+        let sut = makeSUT(startingSet: startSet, nextSet: createAnyTimerSet())
 
         expect(sut: sut,
                toDeliver: [startSet.addingElapsedSeconds(1), startSet.addingElapsedSeconds(2)],
@@ -76,7 +91,7 @@ final class FoundationTimerCountdownTests: XCTestCase {
     
     func test_startTwice_doesNotChangeStateOfRunning() {
         let startSet = createAnyTimerSet()
-        let sut = makeSUT(startingSet: startSet)
+        let sut = makeSUT(startingSet: startSet, nextSet: createAnyTimerSet())
 
         assertsStartCountdownTwiceChangesStateToRunning(sut: sut)
     }
@@ -94,9 +109,10 @@ final class FoundationTimerCountdownTests: XCTestCase {
         XCTAssertEqual(sut.state, .running)
     }
     
-    private func makeSUT(startingSet: LocalElapsedSeconds, file: StaticString = #filePath,
+    private func makeSUT(startingSet: LocalElapsedSeconds, nextSet: LocalElapsedSeconds,
+                         file: StaticString = #filePath,
                          line: UInt = #line) -> FoundationTimerCountdown {
-        let sut = FoundationTimerCountdown(startingSet: startingSet)
+        let sut = FoundationTimerCountdown(startingSet: startingSet, nextSet: nextSet)
         
         trackForMemoryLeak(instance: sut, file: file, line: line)
         
@@ -104,7 +120,9 @@ final class FoundationTimerCountdownTests: XCTestCase {
     }
     
     private func expect(sut: FoundationTimerCountdown, toDeliver deliverExpectation: [LocalElapsedSeconds],
-                        andChangesStateTo expectedState: FoundationTimerCountdown.TimerState) {
+                        andChangesStateTo expectedState: FoundationTimerCountdown.TimerState,
+                        file: StaticString = #filePath,
+                        line: UInt = #line) {
         var receivedElapsedSeconds = [LocalElapsedSeconds]()
         let expectation = expectation(description: "wait for start countdown to deliver time.")
         expectation.expectedFulfillmentCount = deliverExpectation.count
@@ -118,11 +136,15 @@ final class FoundationTimerCountdownTests: XCTestCase {
         wait(for: [expectation], timeout: Double(deliverExpectation.count) + 0.1)
         sut.invalidatesTimer()
 
-        XCTAssertEqual(receivedElapsedSeconds, deliverExpectation)
-        XCTAssertEqual(sut.state, expectedState)
+        XCTAssertEqual(receivedElapsedSeconds, deliverExpectation, file: file, line: line)
+        XCTAssertEqual(sut.state, expectedState, file: file, line: line)
     }
     
     private func createAnyTimerSet(startingFrom startDate: Date = Date()) -> LocalElapsedSeconds {
-        LocalElapsedSeconds(0, startDate: startDate, endDate: startDate.adding(seconds: 1))
+        createTimerSet(0, startDate: startDate, endDate: startDate.adding(seconds: 1))
+    }
+    
+    private func createTimerSet(_ elapsedSeconds: TimeInterval, startDate: Date, endDate: Date) -> LocalElapsedSeconds {
+        LocalElapsedSeconds(elapsedSeconds, startDate: startDate, endDate: endDate)
     }
 }
