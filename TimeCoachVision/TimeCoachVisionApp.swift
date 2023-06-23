7a73e8e714be37ff450cf286e8063874dd17ab5a
Dan Torres <danstorre@gmail.com>
2023-06-23 09:50:35 -0400

    adds Vision OS app

---
+++ TimeCoachVision/TimeCoachVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCoachVisionApp.swift
+//  TimeCoachVision
+//
+//  Created by Daniel Torres on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeCoachVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
