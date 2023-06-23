e881ea56235f06337083dd3b31ca66284b8bc13f
Dan Torres <danstorre@gmail.com>
2023-06-23 14:59:21 -0400

    composes `TimeCoachVisionApp`

--- TimeCoachVision/TimeCoachVisionApp.swift
+++ TimeCoachVision/TimeCoachVisionApp.swift
@@ -1,17 +1,26 @@
-//
-//  TimeCoachVisionApp.swift
-//  TimeCoachVision
-//
-//  Created by Daniel Torres on 6/23/23.
-//
+//  TimeCoach Watch App
+//  Created by Daniel Torres on 12/1/22.
 
 import SwiftUI
+import TimeCoachVisionOS
+import LifeCoach
+import Combine
 
 @main
-struct TimeCoachVisionApp: App {
+struct TimeCoach_Watch_AppApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+    var timerView: TimerView
+    private var root: TimeCoachRoot
+    
+    init() {
+        let root = TimeCoachRoot()
+        self.root = root
+        self.timerView = root.createTimer()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            timerView
         }
     }
 }
