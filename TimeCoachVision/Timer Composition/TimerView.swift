a34ccc4e7a6eb184346e12a27c9b4124495fab1b
Dan Torres <danstorre@gmail.com>
2023-06-23 14:45:39 -0400

    removes tests since ViewInspector is not supporting VisionOS. Add required composition Objects to VisionOS

---
+++ TimeCoachVision/Timer Composition/TimerView.swift
@@ -0,0 +1,53 @@
+import LifeCoach
+import TimeCoachVisionOS
+import SwiftUI
+
+public struct TimerView: View {
+    public var timerWithTimeLine: TimerTextTimeLine?
+    public var timerWithoutTimeLine: TimerText?
+    public var controls: TimerControls
+    var withTimeLine = true
+    
+    public init(
+        timerViewModel: TimerViewModel,
+        togglePlayback: (() -> Void)? = nil,
+        skipHandler: (() -> Void)? = nil,
+        stopHandler: (() -> Void)? = nil,
+        customFont: String? = nil,
+        withTimeLine: Bool = false
+    ) {
+        if withTimeLine {
+            self.timerWithTimeLine = TimerTextTimeLine(timerViewModel: timerViewModel, customFont: customFont)
+        } else {
+            self.timerWithoutTimeLine = TimerText(timerViewModel: timerViewModel, mode: .full, customFont: customFont)
+        }
+        
+        self.withTimeLine = withTimeLine
+        self.controls = TimerControls(togglePlayback: togglePlayback, skipHandler: skipHandler, stopHandler: stopHandler)
+    }
+    
+    public var body: some View {
+        VStack {
+            if withTimeLine {
+                timerWithTimeLine
+            } else {
+                timerWithoutTimeLine
+            }
+            controls
+        }
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView(timerViewModel: TimerViewModel())
+    }
+}
+
+extension TimerView {
+    public static let togglePlaybackButtonIdentifier: Int = 2
+    
+    public static let skipButtonIdentifier: Int = 1
+    
+    public static let stopButtonIdentifier: Int = 0
+}
