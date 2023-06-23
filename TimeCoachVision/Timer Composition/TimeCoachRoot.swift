399ce613b1ecc304f4431814b3c5088280f8c7ca
Dan Torres <danstorre@gmail.com>
2023-06-23 14:58:58 -0400

    deletes unneeded code

--- TimeCoachVision/Timer Composition/TimeCoachRoot.swift
+++ TimeCoachVision/Timer Composition/TimeCoachRoot.swift
@@ -6,23 +6,12 @@ import UserNotifications
 
 class TimeCoachRoot {
     private var timerCoundown: TimerCountdown
-    private var timerSave: TimerSave
-    private var timerLoad: TimerLoad
     
     init() {
         let pomodoro = PomodoroLocalTimer(startDate: .now,
                                           primaryInterval: .pomodoroInSeconds,
                                           secondaryTime: .breakInSeconds)
-        self.timerSave = pomodoro
         self.timerCoundown = pomodoro
-        self.timerLoad = pomodoro
-    }
-    
-    convenience init(timerCoundown: TimerCountdown, timerState: TimerSave & TimerLoad) {
-        self.init()
-        self.timerCoundown = timerCoundown
-        self.timerSave = timerState
-        self.timerLoad = timerState
     }
     
     func createTimer(withTimeLine: Bool = true) -> TimerView {
