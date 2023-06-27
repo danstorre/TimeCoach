638c20bcea7297faf7758e99de0458830d178cf0
Dan Torres <danstorre@gmail.com>
2023-06-26 20:43:46 -0400

    created a "LifeCoachWatchOSUIPreview Watch App" Target so watch OS UI can be used as preview.

---
+++ LifeCoachWatchOS/Timer Feature/UI/LifeCoachWatchOSUIPreviewApp.swift
@@ -0,0 +1,24 @@
+//
+//  LifeCoachWatchOSUIPreviewApp.swift
+//  LifeCoachWatchOSUIPreview Watch App
+//
+//  Created by Daniel Torres on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct LifeCoachWatchOSUIPreview_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
+struct ContentView: View {
+    var body: some View {
+        Text("any")
+    }
+}
