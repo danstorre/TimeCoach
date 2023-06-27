1ca0fbfe3073f12897866cc4956568e3da55fc0c
Dan Torres <danstorre@gmail.com>
2023-06-26 22:19:04 -0400

    new timer factory method in `TimeCoachRoot`

--- TimeCoach Watch App/Timer Composition/TimeCoachRoot.swift
+++ TimeCoach Watch App/Timer Composition/TimeCoachRoot.swift
@@ -10,6 +10,8 @@ class TimeCoachRoot {
     private var timerLoad: TimerLoad
     private var notificationDelegate: UNUserNotificationCenterDelegate
     
+    private var regularTimer: RegularTimer?
+    
     init() {
         let pomodoro = PomodoroLocalTimer(startDate: .now,
                                           primaryInterval: .pomodoroInSeconds,
@@ -39,6 +41,51 @@ class TimeCoachRoot {
         )
     }
     
+    func newCreateTimer(withTimeLine: Bool = true) -> TimerView {
+        let date = Date()
+        let foundationTimerCountdown = FoundationTimerCountdown(startingSet: .pomodoroSet(date: date),
+                                                                nextSet: .breakSet(date: date))
+        let currentSubject = CurrentValueSubject<ElapsedSeconds, Error>(ElapsedSeconds(0, startDate: date, endDate: date))
+        regularTimer = PomodoroTimer(timer: foundationTimerCountdown, timeReceiver: { result in
+            switch result {
+            case let .success(seconds):
+                currentSubject.send(seconds)
+            case let .failure(error):
+                currentSubject.send(completion: .failure(error))
+            }
+        })
+        
+        let passSubject = Deferred { PassthroughSubject<Void, Error>() }
+        let currentValuePublisher = Deferred { currentSubject }
+        
+        return TimerViewComposer.newCreateTimer(
+            customFont: CustomFont.timer.font,
+            playPublisher: currentValuePublisher
+                .merge(with: passSubject
+                    .map({ [regularTimer] _ in
+                        regularTimer?.start()
+                    }).map({ _ in
+                        return ElapsedSeconds(0, startDate: date, endDate: date)
+                    }).eraseToAnyPublisher())
+                .eraseToAnyPublisher(),
+            skipPublisher: currentValuePublisher
+                .merge(with: passSubject
+                    .map({ [regularTimer]_ in
+                        regularTimer?.skip()
+                    }).map({ _ in
+                        return ElapsedSeconds(0, startDate: date, endDate: date)
+                    }).eraseToAnyPublisher())
+                .eraseToAnyPublisher(),
+            stopPublisher: passSubject.map({ [regularTimer] _ in
+                regularTimer?.stop()
+            }).eraseToAnyPublisher(),
+            pausePublisher: passSubject.map({ [regularTimer] _ in
+                regularTimer?.pause()
+            }).eraseToAnyPublisher(),
+            withTimeLine: withTimeLine
+        )
+    }
+    
     func goToBackground() {
         timerSave.saveTime(completion: { time in
             UserNotificationDelegate.registerNotificationOn(remainingTime: time)
@@ -47,5 +94,15 @@ class TimeCoachRoot {
     
     func goToForeground() {
         timerLoad.loadTime()
+    }
+}
+
+extension LocalElapsedSeconds {
+    static func pomodoroSet(date: Date) -> LocalElapsedSeconds {
+        LocalElapsedSeconds(0, startDate: date, endDate: date.adding(seconds: .pomodoroInSeconds))
+    }
+    
+    static func breakSet(date: Date) -> LocalElapsedSeconds {
+        LocalElapsedSeconds(0, startDate: date, endDate: date.adding(seconds: .breakInSeconds))
     }
 }
