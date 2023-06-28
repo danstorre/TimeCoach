9a325e35fd92eeb9b68bb98f0af11fbb34128786
Dan Torres <danstorre@gmail.com>
2023-06-28 17:30:20 -0400

    delivered on none mode sets timer to empty.

--- LifeCoachTests/TimerPresentationTests.swift
+++ LifeCoachTests/TimerPresentationTests.swift
@@ -67,6 +67,15 @@ final class TimerPresentationTests: XCTestCase {
         XCTAssertEqual(sut.timerString, "00:00")
     }
     
+    func test_delivered_OnNoneMode_setsTimerStringToEmpty() {
+        let sut = TimerViewModel()
+        sut.mode = .none
+        
+        sut.delivered(elapsedTime: elapsedSecondsFromPomodoro(0))
+        
+        XCTAssertEqual(sut.timerString, "--:--")
+    }
+    
     private func elapsedSecondsFromPomodoro(_ seconds: TimeInterval) -> ElapsedSeconds {
         let now = Date.now
         return ElapsedSeconds(seconds, startDate: now, endDate: now.adding(seconds: .pomodoroInSeconds))
