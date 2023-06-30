51aa1275005dde19c13adfde8383c3035a519d22
Dan Torres <danstorre@gmail.com>
2023-06-29 20:06:23 -0400

    creates timecontrol `viewModel`

--- LifeCoachWatchOS/Timer Feature/UI/TimerControls.swift
+++ LifeCoachWatchOS/Timer Feature/UI/TimerControls.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import LifeCoach
 
 public struct TimerControls: View {
     @Environment(\.isLuminanceReduced) var isLuminanceReduced
@@ -6,16 +7,26 @@ public struct TimerControls: View {
     private var togglePlayback: (() -> Void)?
     private var skipHandler: (() -> Void)?
     private var stopHandler: (() -> Void)?
+    private let viewModel: ControlsViewModel
+    
+    private var playing: Bool {
+        switch viewModel.state {
+        case .pause: return false
+        case .play: return true
+        }
+    }
     
     public init(
-         togglePlayback: (() -> Void)? = nil,
-         skipHandler: (() -> Void)? = nil,
-         stopHandler: (() -> Void)? = nil,
-         customFont: String? = nil
+        viewModel: ControlsViewModel = ControlsViewModel(),
+        togglePlayback: (() -> Void)? = nil,
+        skipHandler: (() -> Void)? = nil,
+        stopHandler: (() -> Void)? = nil,
+        customFont: String? = nil
     ) {
         self.togglePlayback = togglePlayback
         self.skipHandler = skipHandler
         self.stopHandler = stopHandler
+        self.viewModel = viewModel
     }
     
     public var body: some View {
@@ -28,20 +39,55 @@ public struct TimerControls: View {
                 Image(systemName: "forward.end.fill")
             }
             
-            Button.init(action: {
-                togglePlayback?()
-                print("toggle")
-            }) {
-                Image(systemName: "playpause.fill")
-            }
-            .buttonStyle(.borderedProminent)
+            ToggleButton(togglePlayback: togglePlayback, playing: playing)
         }
         .opacity(isLuminanceReduced ? 0.0 : 1.0)
     }
 }
 
+private struct ToggleButton: View {
+    private let togglePlayback: (() -> Void)?
+    private let playing: Bool
+    
+    init(togglePlayback: (() -> Void)?, playing: Bool) {
+        self.togglePlayback = togglePlayback
+        self.playing = playing
+    }
+    
+    var body: some View {
+        if playing {
+            Button.init(action: {
+                togglePlayback?()
+            }) {
+                Image(systemName: "playpause.fill")
+            }
+            .buttonStyle(.borderedProminent)
+        } else {
+            Button.init(action: {
+                togglePlayback?()
+            }) {
+                Image(systemName: "playpause.fill")
+            }
+            .buttonStyle(.automatic)
+        }
+    }
+}
+
 struct TimerControlsPreviews: PreviewProvider {
+    static func playing() -> ControlsViewModel {
+        let vm = ControlsViewModel()
+        vm.state = .play
+        return vm
+    }
+    
     static var previews: some View {
-        TimerControls()
+        Group {
+            VStack {
+                TimerControls(viewModel: Self.playing())
+                
+                TimerControls()
+            }
+            
+        }
     }
 }
