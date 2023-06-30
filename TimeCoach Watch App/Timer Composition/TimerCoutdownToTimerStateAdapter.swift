7607ee8b9823de9c088b533fb824f3c55585d5c3
Dan Torres <danstorre@gmail.com>
2023-06-30 09:33:30 -0400

    move `TimerCoutdownToTimerStateAdapter` to its own file.

---
+++ TimeCoach Watch App/Timer Composition/TimerCoutdownToTimerStateAdapter.swift
@@ -0,0 +1,67 @@
+import Foundation
+import LifeCoach
+import Combine
+
+class TimerCoutdownToTimerStateAdapter: TimerCoutdown, HasTimerState {
+    var isPlaying: Bool {
+        switch timer.state {
+        case .running: return true
+        case .pause, .stop: return false
+        }
+    }
+    
+    private let timer: TimerCoutdown
+    var currentSetElapsedTime: TimeInterval {
+        timer.currentSetElapsedTime
+    }
+    var state: LifeCoach.TimerState {
+        timer.state
+    }
+    
+    @Published private var isRunning = false
+    
+    lazy private(set) var isRunningPublisher: AnyPublisher<Bool, Never>
+      = $isRunning
+      .dropFirst()
+      .eraseToAnyPublisher()
+    
+    init(timer: TimerCoutdown) {
+        self.timer = timer
+    }
+    
+    func startCountdown(completion: @escaping StartCoundownCompletion) {
+        timer.startCountdown { [unowned self] result in
+            self.isRunning = self.isPlaying
+            completion(result)
+        }
+        isRunning = isPlaying
+    }
+    
+    func stopCountdown() {
+        timer.stopCountdown()
+        isRunning = isPlaying
+    }
+    
+    func pauseCountdown() {
+        timer.pauseCountdown()
+        isRunning = isPlaying
+    }
+    
+    func skipCountdown(completion: @escaping SkipCountdownCompletion) {
+        timer.skipCountdown{ [unowned self] result in
+            self.isRunning = self.isPlaying
+            completion(result)
+        }
+        isRunning = isPlaying
+    }
+}
+
+extension TimerCoutdownToTimerStateAdapter {
+    func isPlayingPublisherProvider() -> () -> AnyPublisher<Bool, Never> {
+        {
+            self.$isRunning
+                .dropFirst()
+                .eraseToAnyPublisher()
+        }
+    }
+}
