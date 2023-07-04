be7b7ca1d876cb6f38f599d29fa5bf82846b13ac
Dan Torres <danstorre@gmail.com>
2023-07-04 15:05:05 -0400

    removes previous factory method.

--- TimeCoach Watch App/Timer Composition/TimerViewComposer.swift
+++ TimeCoach Watch App/Timer Composition/TimerViewComposer.swift
@@ -29,59 +29,6 @@ public final class TimerViewComposer {
         return controlsViewModel
     }
     
-    public static func createTimer2(
-        controlsViewModel: ControlsViewModel = ControlsViewModel(),
-        viewModel timerViewModel: TimerViewModel = TimerViewModel(isBreak: false),
-        customFont: String = CustomFont.timer.font,
-        breakColor: Color = .blue,
-        playPublisher: @escaping () -> AnyPublisher<ElapsedSeconds, Error>,
-        skipPublisher: @escaping () -> AnyPublisher<ElapsedSeconds, Error>,
-        stopPublisher: AnyPublisher<Void, Error>,
-        pausePublisher: AnyPublisher<Void, Error>,
-        isPlayingPublisher: @escaping () -> AnyPublisher<Bool,Never>,
-        withTimeLine: Bool,
-        hasPlayerState: HasTimerState
-    ) -> TimerView {
-        let starTimerAdapter = TimerAdapter(loader: playPublisher)
-        starTimerAdapter.presenter = timerViewModel
-        
-        let skipTimerAdapter = TimerAdapter(loader: skipPublisher)
-        skipTimerAdapter.presenter = timerViewModel
-        let skipHandler = Self.handlesSkip(withSkipAdapter: skipTimerAdapter,
-                                           and: timerViewModel)
-        
-        let stopTimerAdapter = TimerVoidAdapter(loader: stopPublisher)
-        
-        let pauseTimerAdapter = TimerVoidAdapter(loader: pausePublisher)
-        
-        let controlsViewModel = Self.subscribeChangesFrom(isPlayingPublisher: isPlayingPublisher,
-                                                          to: controlsViewModel)
-        let toggleStrategy = ToggleStrategy(start: starTimerAdapter.start,
-                                            pause: pauseTimerAdapter.pause,
-                                            skip: skipHandler,
-                                            stop: stopTimerAdapter.stop,
-                                            hasPlayerState: hasPlayerState)
-        
-        let controls = TimerControls(viewModel: controlsViewModel,
-                                     togglePlayback: toggleStrategy.toggle,
-                                     skipHandler: toggleStrategy.skipHandler,
-                                     stopHandler: toggleStrategy.stopHandler)
-        
-        if withTimeLine {
-            let timerWithTimeLine = TimerTextTimeLine(timerViewModel: timerViewModel,
-                                                      breakColor: breakColor,
-                                                      customFont: customFont)
-            
-            return TimerView(timerWithTimeLine: timerWithTimeLine, controls: controls)
-        } else {
-            let timerWithoutTimeLine = TimerText(timerViewModel: timerViewModel,
-                                                 mode: .full,
-                                                 breakColor: breakColor,
-                                                 customFont: customFont)
-            return TimerView(timerWithoutTimeLine: timerWithoutTimeLine, controls: controls)
-        }
-    }
-    
     public static func createTimer(
         controlsViewModel: ControlsViewModel = ControlsViewModel(),
         viewModel timerViewModel: TimerViewModel = TimerViewModel(isBreak: false),
