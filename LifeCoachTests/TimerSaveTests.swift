8be1ac84ab8a1b316bf7af0e96b918dfdbaa88bd
Dan Torres <danstorre@gmail.com>
2023-07-27 19:28:10 -0400

    changes elapsed seconds to state to conform to business requirements.

--- LifeCoachTests/TimerSaveTests.swift
+++ LifeCoachTests/TimerSaveTests.swift
@@ -1,21 +1,31 @@
 import XCTest
 import LifeCoach
 
+struct TimerState {
+    let elapsedSeconds: ElapsedSeconds
+}
+
 class LocalTimer {
     private let store: LocaTimerSpy
     init(store: LocaTimerSpy) {
         self.store = store
     }
     
-    func save(elapsedSeconds: ElapsedSeconds) throws {
+    func save(state: TimerState) throws {
         store.deleteState()
-        store.insert(state: LocalTimerState(localElapsedSeconds: elapsedSeconds.local))
+        store.insert(state: state.local)
     }
 }
 
 extension ElapsedSeconds {
     var local: LocalElapsedSeconds {
         LocalElapsedSeconds(elapsedSeconds, startDate: startDate, endDate: endDate)
+    }
+}
+
+extension TimerState {
+    var local: LocalTimerState {
+        LocalTimerState(localElapsedSeconds: elapsedSeconds.local)
     }
 }
 
@@ -75,36 +85,35 @@ final class TimerSaveStateUseCaseTests: XCTestCase {
     }
     
     func test_save_sendsDeleteStateMessageToStore() {
-        let anyElapsedSeconds = makeAnyLocalElapsedSeconds()
+        let anyTimerState = makeAnyState()
         let (sut, spy) = makeSUT()
         
-        try? sut.save(elapsedSeconds: anyElapsedSeconds.model)
+        try? sut.save(state: anyTimerState.model)
         
         XCTAssertEqual(spy.deleteMessageCount, 1)
     }
     
     func test_save_onStoreDeletionErrorShouldDeliverError() {
-        let anyElapsedSeconds = makeAnyLocalElapsedSeconds()
+        let anyTimerState = makeAnyState()
         let expectedError = anyNSError()
         let (sut, spy) = makeSUT()
         spy.failDeletion(with: expectedError)
         
         do {
-            try sut.save(elapsedSeconds: anyElapsedSeconds.model)
+            try sut.save(state: anyTimerState.model)
         } catch {
             XCTAssertEqual(error as NSError, expectedError)
         }
     }
     
     func test_save_onStoreDeletionSucces_sendMessageInsertionWithCorrectStateToStore() {
-        let anyElapsedSeconds = makeAnyLocalElapsedSeconds()
-        let expectedLocalState = LocalTimerState(localElapsedSeconds: anyElapsedSeconds.local)
+        let anyTimerState = makeAnyState()
         let (sut, spy) = makeSUT()
         spy.completesDeletionSuccessfully()
         
-        try? sut.save(elapsedSeconds: anyElapsedSeconds.model)
+        try? sut.save(state: anyTimerState.model)
         
-        XCTAssertEqual(spy.receivedMessages, [.deleteState, .insert(state: expectedLocalState)])
+        XCTAssertEqual(spy.receivedMessages, [.deleteState, .insert(state: anyTimerState.local)])
     }
     
     // MARK:- Helper Methods
@@ -116,6 +125,16 @@ final class TimerSaveStateUseCaseTests: XCTestCase {
         trackForMemoryLeak(instance: spy, file: file, line: line)
         
         return (sut, spy)
+    }
+    
+    private func makeAnyState(seconds: TimeInterval = 1,
+                              startDate: Date = Date(),
+                              endDate: Date = Date()) -> (model: TimerState, local: LocalTimerState) {
+        let elapsedSeconds = makeAnyLocalElapsedSeconds(seconds: seconds, startDate: startDate, endDate: endDate)
+        let model = TimerState(elapsedSeconds: elapsedSeconds.model)
+        let local = LocalTimerState(localElapsedSeconds: elapsedSeconds.local)
+        
+        return (model, local)
     }
     
     private func makeAnyLocalElapsedSeconds(seconds: TimeInterval = 1,
