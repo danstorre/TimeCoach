a4d57cb399276a8457a29c28a03aabe637c010bc
Dan Torres <danstorre@gmail.com>
2023-07-27 18:23:02 -0400

    sends retrieve message to store.

--- LifeCoachTests/TimerSaveTests.swift
+++ LifeCoachTests/TimerSaveTests.swift
@@ -1,14 +1,23 @@
 import XCTest
+import LifeCoach
 
 class LocalTimer {
     private let store: LocaTimerSpy
     init(store: LocaTimerSpy) {
         self.store = store
     }
+    
+    func save(elapsedSeconds: ElapsedSeconds) {
+        store.retrieveElapsedSeconds()
+    }
 }
 
 class LocaTimerSpy {
-    var retrieveMessageCount = 0
+    private(set) var retrieveMessageCount = 0
+    
+    func retrieveElapsedSeconds() {
+        retrieveMessageCount += 1
+    }
 }
 
 final class TimerSaveStateUseCaseTests: XCTestCase {
@@ -17,5 +26,19 @@ final class TimerSaveStateUseCaseTests: XCTestCase {
         let _ = LocalTimer(store: spy)
         
         XCTAssertEqual(spy.retrieveMessageCount, 0)
+    }
+    
+    func test_save_sendsRetrieveMessageToStore() {
+        let anyElapsedSeconds = makeAnyLocalElapsedSeconds()
+        let spy = LocaTimerSpy()
+        let sut = LocalTimer(store: spy)
+        
+        sut.save(elapsedSeconds: anyElapsedSeconds)
+        
+        XCTAssertEqual(spy.retrieveMessageCount, 1)
+    }
+    
+    private func makeAnyLocalElapsedSeconds(seconds: TimeInterval = 1, startDate: Date = Date(), endDate: Date = Date()) -> ElapsedSeconds {
+        ElapsedSeconds(seconds, startDate: startDate, endDate: endDate)
     }
 }
