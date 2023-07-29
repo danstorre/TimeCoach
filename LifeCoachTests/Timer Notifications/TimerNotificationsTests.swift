e1c7cbc5bbc299f26f20c5c2602eb141bc1389cf
Dan Torres <danstorre@gmail.com>
2023-07-29 18:38:02 -0400

    send message to scheduler on schedule notification on same date as start date of the set  with correct values

--- LifeCoachTests/Timer Notifications/TimerNotificationsTests.swift
+++ LifeCoachTests/Timer Notifications/TimerNotificationsTests.swift
@@ -1,19 +1,56 @@
 import XCTest
+import LifeCoach
 
 class TimerNotificationScheduler {
-    init(scheduler: Spy) {}
+    private let scheduler: Spy
+    
+    init(scheduler: Spy) {
+        self.scheduler = scheduler
+    }
+    
+    func scheduleNotification(from elapsedSeconds: ElapsedSeconds) {
+        scheduler.setSchedule(at: elapsedSeconds.endDate)
+    }
 }
 
 class Spy {
     private(set) var scheduleCallCount = 0
+    private(set) var receivedMessages = [AnyMessage]()
+    
+    enum AnyMessage: Equatable {
+        case scheduleExecution(at: Date)
+    }
+    
+    func setSchedule(at scheduledDate: Date) {
+        scheduleCallCount += 1
+        
+        receivedMessages.append(.scheduleExecution(at: scheduledDate))
+    }
 }
 
 final class TimerNotificationsTests: XCTestCase {
-
     func test_init_doesNotScheduleTimer() {
         let spy = Spy()
         let _ = TimerNotificationScheduler(scheduler: spy)
         
         XCTAssertEqual(spy.scheduleCallCount, 0)
+    }
+    
+    func test_scheduleNotification_onSameDateAsStartDate_shouldSendMessageToSchedulerWithCorrectValues() {
+        let startDate = Date()
+        let endDate = startDate.adding(seconds: 1)
+        let elapsedSeconds = ElapsedSeconds(0, startDate: startDate, endDate: endDate)
+        let spy = Spy()
+        let sut = TimerNotificationScheduler(scheduler: spy)
+        
+        sut.scheduleNotification(from: elapsedSeconds)
+        
+        XCTAssertEqual(spy.receivedMessages, [.scheduleExecution(at: endDate)])
+    }
+}
+
+extension Date {
+    func adding(seconds: TimeInterval) -> Date {
+        self + seconds
     }
 }
