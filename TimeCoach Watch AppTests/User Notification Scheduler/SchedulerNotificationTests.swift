73b618079aa34e5a3292ef9b598171cee8efa61c
Dan Torres <danstorre@gmail.com>
2023-07-30 16:57:19 -0400

    schedules a trigger on remaining time from date given.

--- TimeCoach Watch AppTests/User Notification Scheduler/SchedulerNotificationTests.swift
+++ TimeCoach Watch AppTests/User Notification Scheduler/SchedulerNotificationTests.swift
@@ -3,15 +3,22 @@ import XCTest
 import UserNotifications
 
 class UserNotificationsScheduler {
+    private let currentDate: () -> Date
     private let notificationCenter: NotificationScheduler
     
-    init(with notificationCenter: NotificationScheduler) {
+    init(currentDate: @escaping () -> Date = Date.init, with notificationCenter: NotificationScheduler) {
         self.notificationCenter = notificationCenter
+        self.currentDate = currentDate
     }
     
-    func setSchedule() {
+    func setSchedule(at date: Date) {
         notificationCenter.removeAllDeliveredNotifications()
         notificationCenter.removeAllPendingNotificationRequests()
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let notification = UNNotificationRequest(identifier: "", content: .init(), trigger: trigger)
+        
+        notificationCenter.add(notification)
     }
 }
 
@@ -27,7 +34,7 @@ final class SchedulerNotificationTests: XCTestCase {
         let fake = UNUserNotificationCenterSpy()
         let sut = UserNotificationsScheduler(with: fake)
         
-        sut.setSchedule()
+        sut.setSchedule(at: anyScheduledDate())
         
         XCTAssertEqual(fake.removeAllDeliveredNotificationsCallCount, 1)
     }
@@ -36,15 +43,29 @@ final class SchedulerNotificationTests: XCTestCase {
         let fake = UNUserNotificationCenterSpy()
         let sut = UserNotificationsScheduler(with: fake)
         
-        sut.setSchedule()
+        sut.setSchedule(at: anyScheduledDate())
         
         XCTAssertEqual(fake.removeAllPendingNotificationRequestsCallCount, 1)
+    }
+    
+    func test_schedule_scheduleAtriggerOnRemainingTime() {
+        let currentDate = Date()
+        let fake = UNUserNotificationCenterSpy()
+        let sut = UserNotificationsScheduler(currentDate: { currentDate }, with: fake)
+        
+        let timeScheduled = currentDate.adding(seconds: 1)
+        sut.setSchedule(at: timeScheduled)
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        
+        XCTAssertEqual(fake.receivedNotifications.first?.trigger, trigger)
     }
     
     // MARK: - Helpers
     class UNUserNotificationCenterSpy: NotificationScheduler {
         private(set) var removeAllDeliveredNotificationsCallCount = 0
         private(set) var removeAllPendingNotificationRequestsCallCount = 0
+        private(set) var receivedNotifications = [UNNotificationRequest]()
         
         func removeAllDeliveredNotifications() {
             removeAllDeliveredNotificationsCallCount += 1
@@ -53,10 +74,27 @@ final class SchedulerNotificationTests: XCTestCase {
         func removeAllPendingNotificationRequests() {
             removeAllPendingNotificationRequestsCallCount += 1
         }
+        
+        func add(_ notification: UNNotificationRequest) {
+            receivedNotifications.append(notification)
+        }
+    }
+    
+    private func anyScheduledDate() -> Date {
+        Date().adding(seconds: 1)
     }
 }
 
 protocol NotificationScheduler {
     func removeAllDeliveredNotifications()
     func removeAllPendingNotificationRequests()
+    
+    func add(_ notification: UNNotificationRequest)
+}
+
+
+extension Date {
+    func adding(seconds: TimeInterval) -> Date {
+        self + seconds
+    }
 }
