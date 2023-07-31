88ba75e421fe32036ed4e0fecb6c0bb82ce684a8
Dan Torres <danstorre@gmail.com>
2023-07-31 13:34:03 -0400

    receiving notification executes completion.

--- LifeCoachTests/Timer Notifications/TimerNoticationReceiverTests.swift
+++ LifeCoachTests/Timer Notifications/TimerNoticationReceiverTests.swift
@@ -3,9 +3,14 @@ import XCTest
 
 
 class TimerNotificationReceiver {
+    private let completion: () -> Void
     
     init(completion: @escaping () -> Void) {
-        
+        self.completion = completion
+    }
+    
+    func receiveNotification() {
+        completion()
     }
 }
 
@@ -16,5 +21,15 @@ final class TimerNoticationReceiverTests: XCTestCase {
         let _ = TimerNotificationReceiver(completion: completion)
         
         XCTAssertEqual(executedCompletion, 0)
+    }
+    
+    func test_receivingNotification_executesCompletion() {
+        var executedCompletion = 0
+        let completion: () -> Void = { executedCompletion += 1 }
+        let sut = TimerNotificationReceiver(completion: completion)
+        
+        sut.receiveNotification()
+        
+        XCTAssertEqual(executedCompletion, 1)
     }
 }
