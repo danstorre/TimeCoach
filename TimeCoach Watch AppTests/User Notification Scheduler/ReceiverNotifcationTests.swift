c5d09fef4abb918f4bf618fd5eca24b9b47f0711
Dan Torres <danstorre@gmail.com>
2023-07-31 14:49:10 -0400

    sends mesage to receiver on receiver.

--- TimeCoach Watch AppTests/User Notification Scheduler/ReceiverNotifcationTests.swift
+++ TimeCoach Watch AppTests/User Notification Scheduler/ReceiverNotifcationTests.swift
@@ -1,21 +1,54 @@
-@testable import LifeCoach
 import XCTest
+import UserNotifications
 
 class UserNotificationsReceiver {
+    private let receiver: MockReceiver
+    
     init(receiver: MockReceiver) {
-        
+        self.receiver = receiver
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        receiver.receiveNotification()
     }
 }
 
 class MockReceiver {
     private(set) var receiveCallCount = 0
+    
+    func receiveNotification() {
+        receiveCallCount += 1
+    }
 }
 
-final class ReceiverNotifcationTests: XCTestCase {
+final class ReceiverNotificationTests: XCTestCase {
     func test_init_shouldNotSendMessageToReceiver() {
+        let spy = MockReceiver()
+        let _ = UserNotificationsReceiver(receiver: spy)
+        
+        XCTAssertEqual(spy.receiveCallCount, 0)
+    }
+    
+    func test_receive_sendsMessageToReceiver() {
         let spy = MockReceiver()
         let sut = UserNotificationsReceiver(receiver: spy)
         
-        XCTAssertEqual(spy.receiveCallCount, 0)
+        sut.receiveNotification()
+        
+        XCTAssertEqual(spy.receiveCallCount, 1)
+    }
+}
+
+private final class FakeKeyedArchiver: NSKeyedArchiver {
+    override func decodeObject(forKey _: String) -> Any { "" }
+    override func decodeInt64(forKey key: String) -> Int64 { 0 }
+}
+
+extension UserNotificationsReceiver {
+    func receiveNotification() {
+        let anyNotification = UNNotification(coder: FakeKeyedArchiver())
+        userNotificationCenter(UNUserNotificationCenter.current(),
+                               willPresent: anyNotification!,
+                               withCompletionHandler: { _ in})
     }
 }
