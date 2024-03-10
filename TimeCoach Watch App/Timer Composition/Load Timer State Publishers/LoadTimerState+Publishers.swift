c1ce08a02ff9dc37b77b42d0d97da4a7b759c9e0
Dan Torres <danstorre@gmail.com>
2024-03-10 16:04:35 -0300

    creates `getTimerSetPublisher` factory helper method.

---
+++ TimeCoach Watch App/Timer Composition/Load Timer State Publishers/LoadTimerState+Publishers.swift
@@ -0,0 +1,14 @@
+import Foundation
+import LifeCoach
+import Combine
+
+extension LoadTimerState {
+    func getTimerSetPublisher() -> AnyPublisher<TimerSet, Error> {
+        return Deferred {
+            Just(())
+                .tryMap { try load() }
+                .compactMap { $0?.timerSet }
+        }
+        .eraseToAnyPublisher()
+    }
+}
