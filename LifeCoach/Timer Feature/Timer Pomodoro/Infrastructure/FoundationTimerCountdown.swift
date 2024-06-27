00acd7ac9c9a51af3e2e96c106b76035e970dc28
Dan Torres <danstorre@gmail.com>
2024-06-27 20:45:17 -0300

    prevents 'DispatchSourceTimer' to fail by canceling asynchronously instead.

--- LifeCoach/Timer Feature/Timer Pomodoro/Infrastructure/FoundationTimerCountdown.swift
+++ LifeCoach/Timer Feature/Timer Pomodoro/Infrastructure/FoundationTimerCountdown.swift
@@ -102,9 +102,7 @@ public final class FoundationTimerCountdown: TimerCountdown {
     }
     
     public func invalidatesTimer() {
-        currentTimer?.setEventHandler {}
         currentTimer?.cancel()
-        currentTimer = nil
     }
     
     /// Suspends underlined currentTimer if set. a.k.a `DispatchSourceTimer`.
@@ -118,10 +116,7 @@ public final class FoundationTimerCountdown: TimerCountdown {
     }
     
     deinit {
-        currentTimer?.setEventHandler {}
         currentTimer?.cancel()
-        currentTimer?.resume()
-        currentTimer = nil
     }
 }
 
