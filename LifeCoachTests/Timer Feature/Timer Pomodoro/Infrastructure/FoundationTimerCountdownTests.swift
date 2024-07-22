131896b2798139bb7bf5c097abfb6c4ddd541140
Dan Torres <danstorre@gmail.com>
2024-07-22 18:41:53 -0300

    removes unneeded test.

--- LifeCoachTests/Timer Feature/Timer Pomodoro/Infrastructure/FoundationTimerCountdownTests.swift
+++ LifeCoachTests/Timer Feature/Timer Pomodoro/Infrastructure/FoundationTimerCountdownTests.swift
@@ -32,23 +32,6 @@ final class FoundationTimerCountdownTests: XCTestCase {
         }
     }
     
-    func test_start_deliversCorrectTimerValues() {
-        let fixedDate = Date()
-        let startSet1 = createAnyTimerSet()
-        let startSet2 = createTimerSet(0, startDate: fixedDate, endDate: fixedDate.adding(seconds: 0.002))
-        
-        let samples: [(startSet: TimerCountdownSet, expected: [TimerCountdownSet])] = [
-            (startSet: startSet1, expected: [startSet1, startSet1.adding(0.001)]),
-            (startSet: startSet2, expected: [startSet2, startSet2.adding(0.001), startSet2.adding(0.002)])
-        ]
-        
-        samples.forEach { sample in
-            let sut = makeSUT(startingSet: sample.startSet, nextSet: createAnyTimerSet())
-            
-            starts(sut: sut, expectingToDeliver: sample.expected)
-        }
-    }
-    
     func test_startTwice_doesNotChangeStateOfRunning() {
         let startSet = createAnyTimerSet()
         let sut = makeSUT(startingSet: startSet, nextSet: createAnyTimerSet())
